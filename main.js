1d4a2f90e0ca23a2e2003d62de6ea175eca5e827
Mr Jim <89702301+JamesTK42@users.noreply.github.com>
2024-01-08 22:49:55 +0000

    no

---
+++ main.js
@@ -0,0 +1,7 @@
+// Get today's date
+let today = new Date();
+let date = today.getDate() + '/' + (today.getMonth()+1);
+
+// Print the message and the date
+function codingSignOff() {
+alert("coded " + date + " h/e JC");}
