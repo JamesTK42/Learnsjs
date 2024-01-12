48efc045febc7f97a7816b579b297f2e26eff76f
Mr Jim <89702301+JamesTK42@users.noreply.github.com>
2024-01-12 08:33:07 +0000

    to

--- main.js
+++ main.js
@@ -2,6 +2,13 @@
 let today = new Date();
 let date = today.getDate() + '/' + (today.getMonth()+1);
 
+let init = document.getElementById('sign_init').textContent;
+
 // Print the message and the date
 function codingSignOff() {
-alert("coded " + date + " h/e JC");}
+// alert("coded " + date + " h/e ");
+alert(init);
+
+
+
+}
