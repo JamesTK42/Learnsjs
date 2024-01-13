7ebbe58b8979b2c698d3ca389d66298c4d5f658b
Mr Jim <89702301+JamesTK42@users.noreply.github.com>
2024-01-13 21:01:13 +0000

    love

--- main.js
+++ main.js
@@ -2,13 +2,28 @@
 let today = new Date();
 let date = today.getDate() + '/' + (today.getMonth()+1);
 
-let init = document.getElementById('sign_init').textContent;
 
 // Print the message and the date
 function codingSignOff() {
-// alert("coded " + date + " h/e ");
-alert(init);
+    var items=document.getElementsByName('codesource');
+    var selectedItems="";
+    for(var i=0; i<items.length; i++){
+        if(items[i].type=='checkbox' && items[i].checked==true)
+            selectedItems+=items[i].value+"/";
+    }
 
+
+
+
+// var init = document.getElementById("signInit").value;
+var init = document.querySelector('input[name="coders_initials"]:checked').value;
+
+// var codesource = document.querySelector('input[name="codesource"]:checked').value;
+
+let finalSign = ("coded " + date + " " + selectedItems + " " + init);
+
+document.getElementById("signPrint").innerHTML = finalSign;
 
 
 }
+
