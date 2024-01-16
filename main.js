45534b2af54a5cadad288d63d2fca71de1fa6d4f
Mr Jim <89702301+JamesTK42@users.noreply.github.com>
2024-01-16 20:40:58 +0000

    the

--- main.js
+++ main.js
@@ -1,26 +1,27 @@
 // Get today's date
 let today = new Date();
-let date = today.getDate() + '/' + (today.getMonth()+1);
-
+let date = today.getDate() + "/" + (today.getMonth() + 1);
 
 // Print the message and the date
 function codingSignOff() {
-    var items=document.getElementsByName('codesource');
-    var selectedItems="";
-    for(var i=0; i<items.length; i++){
-        if(items[i].type=='checkbox' && items[i].checked==true)
-            selectedItems+=items[i].value + "/";
-    }
+  var items = document.getElementsByName("codesource");
+  var selectedItems = "";
+  for (var i = 0; i < items.length; i++) {
+    if (items[i].type == "checkbox" && items[i].checked == true)
+      selectedItems += items[i].value + "/";
+  }
 
-cdenm = selectedItems.slice(0,-1);
+  cdenm = selectedItems.slice(0, -1);
 
-var init = document.querySelector('input[name="coders_initials"]:checked').value;
+  var init = document.querySelector(
+    'input[name="coders_initials"]:checked'
+  ).value;
 
+  let finalSign = "coded " + date + " " + cdenm + " " + init;
+  // somehow add - "/" to remove / from end
+  document.getElementById("signPrint").innerHTML = finalSign;
 
-let finalSign = ("coded " + date + " " + cdenm + " " + init);
-// somehow add - "/" to remove / from end
-document.getElementById("signPrint").innerHTML = finalSign;
-
-
+  navigator.clipboard.writeText(finalSign);
+  alert("coppied to clipboard")
 }
 
