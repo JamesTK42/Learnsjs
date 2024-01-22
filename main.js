53caf9aceecafb9a2544b96ef8bf682e92614714
Mr Jim <89702301+JamesTK42@users.noreply.github.com>
2024-01-22 18:17:19 +0000

    so

--- main.js
+++ main.js
@@ -4,14 +4,10 @@ let date = today.getDate() + "/" + (today.getMonth() + 1);
 
 // If other (radio) selected, hide/make visible input text
 function otherCheck() {
-  if (document.getElementById('Other').checked) {
-      document.getElementById('ifOther').style.visibility = 'visible';
-  }
-  else document.getElementById('ifOther').style.visibility = 'hidden';
-
-
+  if (document.getElementById("Other").checked) {
+    document.getElementById("ifOther").style.visibility = "visible";
+  } else document.getElementById("ifOther").style.visibility = "hidden";
 }
-
 
 function codingSignOff() {
   var items = document.getElementsByName("codesource");
@@ -21,27 +17,33 @@ function codingSignOff() {
       selectedItems += items[i].value + "/";
   }
 
-  
   cdenm = selectedItems.slice(0, -1);
 
-  // var init = document.querySelector(
-  //   'input[name="coders_initials"]:checked'
-  // ).value;
 
-    if (document.getElementById('Other').checked){
-      var init = document.getElementById("ifOther").value;
-    }
-    else{var init = document.querySelector(
+
+  if (document.getElementById("Other").checked) {
+    var init = document.getElementById("ifOther").value;
+  } else {
+    var init = document.querySelector(
       'input[name="coders_initials"]:checked'
-    ).value;}
-  let finalSign = "coded " + date + " " + cdenm + " " + init;
+    ).value;
+  }
+
+  if (document.getElementById("dupe").checked){
+var dupe = " " + document.querySelector(
+  'input[name="duplicate"]:checked'
+).value
+  } else{var dupe = " ";}
+
+
+
+  let finalSign = "Coded " + date + " " + cdenm + " " + init + dupe;
 
   // Prints coding sign off to html
-  // document.getElementById("signPrint").innerHTML = finalSign;
+  document.getElementById("signPrint").innerHTML = finalSign;
 
   // copy result to clipboard
   navigator.clipboard.writeText(finalSign);
   // alert("coppied to clipboard")
-
 }
 
